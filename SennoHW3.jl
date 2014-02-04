039959b317691e44d10f75174e8db593651272e8
sennosays <nbs5044@psu.edu>
2014-02-03 19:02:36 -0500

    ASCII cleanup
    Just cleaned up some ways the functions were called. Tried implementing
    HDF5 but appeared to have a problem with adding the package. Going home
    to eat dinner.

--- SennoHW3.jl
+++ SennoHW3.jl
@@ -1,3 +1,5 @@
+#using HDF5, JLD
+
 #fucntion that times how long it takes to output a string of data 
 #to the common output 
 function time_STDOUT(my_data::Array)
@@ -9,19 +11,20 @@ function time_STDOUT(my_data::Array)
 end 
 
 #function that times the writing to a file of data in ASCII
-function time_file_write(my_data::Array,file_name = "my_data.txt"::String)
-	 file_name = *("Desktop/HPC/HW3/",file_name,".txt"); 
-	 println(file_name); 
+function time_file_write_ASCII(my_data::Array,file_name = "my_data.txt"::String)
+	 file_name = *(file_name,".txt"); 
 	 tic()
 	 writecsv(file_name,my_data); 
 	 toc()
 end 
 
 #fucntion that times the read from a file of data in ASCII
-function time_file_read!(my_data::Array,file_name = "my_data"::String)
-	 file_name = *("Desktop/HPC/HW3/",file_name,".txt"); 
+function time_file_read_ASCII(file_name = "my_data"::String)
+	 file_name = *(file_name,".txt"); 
 	 tic(); 
 	 readcsv(file_name);
 	 toc(); 
 end
+
+
 
